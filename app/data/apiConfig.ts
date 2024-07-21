9988e1966e92535da79d4be4f6dce6b82b353727
Nikita Smolianitski <n.smolianitski@gmail.com>
2024-07-21 22:35:44 +0300

    Change backend port to 8080

--- app/data/apiConfig.ts
+++ app/data/apiConfig.ts
@@ -1,5 +1,5 @@
 const configuration = {
-  API_URL: "http://localhost:3000/api/v1",
+  API_URL: "http://localhost:8080/api/v1",
   GET_INGREDIENTS: "/ingredients",
   GET_MEALS: "/recipes",
   GET_MEALLISTS: "/meal-lists",
